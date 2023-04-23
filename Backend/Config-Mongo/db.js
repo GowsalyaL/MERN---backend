fcc4a47359d2185f810a6be58eaa294b763a69e9
GowsalyaL <118992727+GowsalyaL@users.noreply.github.com>
2023-04-23 21:02:55 +0530

    Update db.js

--- Backend/Config-Mongo/db.js
+++ Backend/Config-Mongo/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
+MONGO_URI = "mongodb+srv://gowsalyanallasivam:Lovely@cluster0.erlnfxt.mongodb.net/Mern-project?retryWrites=true&w=majority"
+
 const connectDb = async () =>{
     try {
 
-        const connect = await mongoose.connect (process.env.MONGO_URI)
+        const connect = await mongoose.connect (MONGO_URI)
         console.log(`Mongo connection: ${connect.connection.host}`.cyan.underline  );
     } catch (error) {
         console.log(error);
